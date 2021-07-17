2fa40981c94a52d6b00c0cfcb05d477af968a3fb
Taeuk Gwak <gwak2837@kakao.com>
2021-07-17 13:16:35 +0900

    Add tests page

---
+++ src/pages/tests/index.tsx
@@ -0,0 +1,28 @@
+import NavigationLayout from 'src/components/layouts/NavigationLayout'
+import PageHead from 'src/components/layouts/PageHead'
+import useSwr from 'swr'
+import { Response } from 'src/pages/api/tests'
+import { fetcher } from 'src/utils/commons'
+import ClientSideLink from 'src/components/atoms/ClientSideLink'
+
+const description = '모의고사 목록을 확인할 수 있어요.'
+
+function TestsPage() {
+  const { data, error } = useSwr<Response>('/api/tests', fetcher)
+
+  return (
+    <PageHead title="수능 모의고사 - 모의고사 목록" description={description}>
+      <NavigationLayout>
+        {data?.map((test) => (
+          <div key={test.id}>
+            <ClientSideLink href={`/tests/${test.name.replace(/ /g, '-')}`}>
+              {test.name}
+            </ClientSideLink>
+          </div>
+        ))}
+      </NavigationLayout>
+    </PageHead>
+  )
+}
+
+export default TestsPage
