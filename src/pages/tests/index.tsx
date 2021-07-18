6ec951f3143622b3550f20e513ba853c16f32c13
Taeuk Gwak <gwak2837@kakao.com>
2021-07-18 12:24:28 +0900

    Make test list bigger

--- src/pages/tests/index.tsx
+++ src/pages/tests/index.tsx
@@ -4,6 +4,13 @@ import useSwr from 'swr'
 import { Response } from 'src/pages/api/tests'
 import { fetcher } from 'src/utils/commons'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
+import styled from 'styled-components'
+
+const Li = styled.li`
+  text-align: center;
+  font-size: 2rem;
+  padding: 1rem;
+`
 
 const description = '여러 수능 모의고사를 확인할 수 있어요.'
 
@@ -13,17 +20,21 @@ function TestsPage() {
   return (
     <PageHead title="수능 모의고사 - 모의고사 목록" description={description}>
       <NavigationLayout>
-        {data
-          ? data.map((test) => (
-              <div key={test.id}>
+        {data ? (
+          <ul>
+            {data.map((test) => (
+              <Li key={test.id}>
                 <ClientSideLink href={`/tests/${test.name.replace(/ /g, '-')}`}>
                   {test.name}
                 </ClientSideLink>
-              </div>
-            ))
-          : error
-          ? '네트워크 요청 오류'
-          : '모의고사 목록 불러오는 중...'}
+              </Li>
+            ))}
+          </ul>
+        ) : error ? (
+          '네트워크 요청 오류'
+        ) : (
+          '모의고사 목록 불러오는 중...'
+        )}
       </NavigationLayout>
     </PageHead>
   )
